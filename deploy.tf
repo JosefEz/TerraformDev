647b200f2ab284b2114b13cbb404214d2e9721f6
EC2 Default User <ec2-user@ip-172-31-16-136.us-east-2.compute.internal>
2022-05-03 03:59:58 +0000

    Terraform VPC, IGW, ROUTING TABLE, and SECURITY GROUP Creation

---
+++ deploy.tf
@@ -0,0 +1,95 @@
+/* Assign a Cloud Provider*/
+
+provider "aws" {}
+
+
+/*List necessary variables*/
+
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip{}
+
+/* Create VPC Resource and Assign the VPC_CIDR_BLOCK Variable*/
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+      Name: "${var.env_prefix}-vpc"
+  }
+}
+
+/* Create SUBNET Resource and Assign the SUBNET_CIDR_BLOCK Variable*/
+
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name: "${var.env_prefix}-subnet-1"
+  }
+}
+
+
+/* Create Internet Gateway  Resource and Attach the VPC resource ID*/
+
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+      Name = "${var.env_prefix}-igw"
+    }
+  
+ }
+
+
+/* Create Routing Table  Resource and Attach the Default VPC Routing Table*/
+/* Also Attache the Internet Gateway Including the Allow all Route */
+
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+  tags = {
+    Name: "${var.env_prefix}-main-rtb"
+  }
+}
+
+/* Use the Default Security Group Inside the VPC */
+
+resource "aws_default_security_group" "default-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+/* Create Ingress for SSH and NGINX Protocol and Assign the My_ip Variable*/
+
+    ingress  {
+      cidr_blocks = [ var.my_ip ]
+      from_port = 22
+      protocol = "tcp"
+      to_port = 22
+    } 
+
+    ingress   {
+      cidr_blocks = [ "0.0.0.0/0" ]
+      from_port = 8080
+      protocol = "tcp"
+      to_port = 8080
+    }
+
+/* Create Egress for Any Network and Any Protcol */ 
+    egress {
+       cidr_blocks = [ "0.0.0.0/0" ]
+      from_port = 0
+      protocol = "-1"
+      to_port = 0
+      prefix_list_ids = []
+    }
+
+    tags = {
+      Name: "${var.env_prefix}-sg"
+    }
+}
+
